21235a5aaf89739fc5dbfcd0561ab2eb9fab957e
Martynas Zilinskas <hello@martynas.me>
2025-02-19 03:37:59 +0200

    Format/lint fixed errors.

--- packages/dotconfig/src/errors.ts
+++ packages/dotconfig/src/errors.ts
@@ -1,33 +1,36 @@
 import type { StandardSchemaV1 } from "@standard-schema/spec";
 
-export class ConfigurationError extends Error { }
-export class ConfigurationAggregateError extends AggregateError { }
+export class ConfigurationError extends Error {}
+export class ConfigurationAggregateError extends AggregateError {}
 
 export class ParseError extends ConfigurationError implements StandardSchemaV1.Issue {
-    constructor(message: string, public path?: ReadonlyArray<PropertyKey | StandardSchemaV1.PathSegment>) {
-        super(message);
-    }
+  constructor(
+    message: string,
+    public path?: ReadonlyArray<PropertyKey | StandardSchemaV1.PathSegment>,
+  ) {
+    super(message);
+  }
 }
 
 export interface ParseErrorsData {
-    configurationPath: string;
+  configurationPath: string;
 }
 
 export class ParseErrors extends ConfigurationAggregateError {
-    constructor(issues: StandardSchemaV1.FailureResult["issues"], data: ParseErrorsData) {
-        super(issues.map((issue) => new ParseError(issue.message, issue.path)));
-        this.message = `Configuration parse errors at "${data.configurationPath}" path.`;
-    }
+  constructor(issues: StandardSchemaV1.FailureResult["issues"], data: ParseErrorsData) {
+    super(issues.map((issue) => new ParseError(issue.message, issue.path)));
+    this.message = `Configuration parse errors at "${data.configurationPath}" path.`;
+  }
 }
 
 export class ProviderRegistryEmptyError extends ConfigurationError {
-    constructor() {
-        super("No providers are available.");
-    }
+  constructor() {
+    super("No providers are available.");
+  }
 }
 
 export class SectionNotFoundError extends ConfigurationError {
-    constructor(path: string) {
-        super(`No configuration section found with the key "${path}".`);
-    }
+  constructor(path: string) {
+    super(`No configuration section found with the key "${path}".`);
+  }
 }
